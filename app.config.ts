121b7d88cfcf9b1c3d98fe7b761aeace23538ee8
JangInChan <simsim051128@gmail.com>
2024-11-28 11:51:47 +0900

    ios용 추가

--- app.config.ts
+++ app.config.ts
@@ -15,20 +15,19 @@ export default {
       backgroundColor: "#ffffff",
     },
     ios: {
+      bundleIdentifier: "com.chosun.eartalk", // iOS Bundle Identifier 설정
+      buildNumber: "2", // iOS Build Number 설정
       supportsTablet: true,
       infoPlist: {
         NSMicrophoneUsageDescription: "앱에서 마이크를 사용하여 음성 인식을 합니다.",
       },
     },
     android: {
-      package: "com.chosun.eartalk",
-      versionCode: 2,
+      package: "com.chosun.eartalk", // Android 패키지 이름
+      versionCode: 2, // Android 버전 코드
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
-      },
-      ios: {
-        "buildNumber": "2"
       },
       permissions: ["RECORD_AUDIO"],
       useBridgeless: false,
@@ -37,7 +36,7 @@ export default {
     extra: {
       API_BASE_URL: process.env.API_BASE_URL || "https://eartalk.site:17004",
       eas: {
-        projectId: "a5c651d4-1f8d-410a-b604-14eede9c4388", // EAS 프로젝트 ID 추가
+        projectId: "a5c651d4-1f8d-410a-b604-14eede9c4388", // EAS 프로젝트 ID
       },
     },
   },
